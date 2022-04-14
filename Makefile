5038fcad9370b163fde5a01060b3fd1d1deb32df
Sirajus Salekin <ssalek14@earlham.edu>
2022-04-14 05:32:47 +0000

    WIP: reading input - got the quantum gate matrix working

--- Makefile
+++ Makefile
@@ -3,12 +3,15 @@ NVCC := $(CUDA_PATH)/bin/nvcc
 CCFLAGS := -O2
 EXTRA_NVCCFLAGS := --cudart=shared
 
-build: quamsimV1 quamsimV2 
+build: quamsimV1 quamsimV2 io
 
 quamsimV2.o:quamsimV2.cu
 	$(NVCC) $(INCLUDES) $(CCFLAGS) $(GENCODE_FLAGS) -o $@ -c $<
 
 quamsimV1.o:quamsimV1.cu
+	$(NVCC) $(INCLUDES) $(CCFLAGS) $(GENCODE_FLAGS) -o $@ -c $<
+
+io.o:io.cu
 	$(NVCC) $(INCLUDES) $(CCFLAGS) $(GENCODE_FLAGS) -o $@ -c $<
 
 quamsimV1: quamsimV1.o
@@ -17,8 +20,8 @@ quamsimV1: quamsimV1.o
 quamsimV2: quamsimV2.o
 	$(NVCC) $(LDFLAGS) $(EXTRA_NVCCFLAGS) $(GENCODE_FLAGS) -o $@ $+ $(LIBRARIES)
 
-run: build
-	$(EXEC) ./vectorAdd
+io: io.o
+	$(NVCC) $(LDFLAGS) $(EXTRA_NVCCFLAGS) $(GENCODE_FLAGS) -o $@ $+ $(LIBRARIES)
 
 clean:
-	rm -f quamsimV1 quamsimV2 *.o
+	rm -f quamsimV1 quamsimV2 io *.o _app_cuda_version_* _cuobjdump_list_ptx_* 
--- io.cu
+++ io.cu
@@ -4,27 +4,33 @@
 
 
 int main(int argc, char **argv){
-
      // How many quantum gate matrices we will have? variable or fixed?
      // Six - fixed
      int NUM_QUANTUM_GATES = 6;
      int QUANTUM_GATE_SIZE = 4;
-
      float gates[NUM_QUANTUM_GATES][QUANTUM_GATE_SIZE];
-                
-     int qgate_elems_size = NUM_QUANTUM_GATES * QUANTUM_GATE_SIZE;
      
-     FILE* in_file = fopen(argv[1], "r"); // read only  
-     float input_elem; 
-     for (int i=0; i < qgate_elems_size; i++){
-        fscanf(in_file, "%f", &input_elem);
-        if (input_elem == EOF){
-            printf("Incorrect input formatting. Abort\n");
-            return 1;
-        }
-        
-        printf("%0.2f\n", input_elem);
+     FILE* in_file = fopen(argv[1], "r");                   // read only  
+     char input_elem[32];                                   // arbitrary length
+
+     for (int i=0; i < NUM_QUANTUM_GATES; i++){
+        for (int j=0; j < QUANTUM_GATE_SIZE; j++){
+            int r = fscanf(in_file, "%s", &input_elem[0]);
+            if (r == EOF){
+                printf("Incorrect input formatting. Abort\n");
+                return 1;
+            }
+            gates[i][j] = atof(input_elem);
+        } 
      }
-    
+
+    //reading from the entered data
+    for (int i=0; i < NUM_QUANTUM_GATES; i++){
+        for (int j=0; j < QUANTUM_GATE_SIZE; j++){
+            printf("%0.3f \n", gates[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
