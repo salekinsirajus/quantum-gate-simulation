ecd15b44ba031fe128b7ee5641c4f81008769ec4
Sirajus Salekin <ssalek14@earlham.edu>
2022-04-15 05:06:41 +0000

    Added back deleted code that was checking whether the input file is readable

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -44,34 +44,40 @@ __global__ void matrix_mul(float *A, float *C, float a, float b, float c, float 
 
 int main(int argc, char **argv){
     /////////////////////////////////////////////////////////////////
-    //                    Reading From Input                       // 
+    //                    Reading From Input                       //
     /////////////////////////////////////////////////////////////////
-     // How many quantum gate matrices we will have? variable or fixed?
-     // Six - fixed
-     int NUM_QUANTUM_GATES = 6;
-     int QUANTUM_GATE_SIZE = 4;
-     float gates[NUM_QUANTUM_GATES][QUANTUM_GATE_SIZE];
-     int T_BITS[NUM_QUANTUM_GATES];
-     
-     FILE* in_file = fopen(argv[1], "r");                   // read only  
-     char input_elem[32];                                   // arbitrary length
+    // How many quantum gate matrices we will have? variable or fixed?
+    // Six - fixed
+    int NUM_QUANTUM_GATES = 6;
+    int QUANTUM_GATE_SIZE = 4;
+    float gates[NUM_QUANTUM_GATES][QUANTUM_GATE_SIZE];
+    int T_BITS[NUM_QUANTUM_GATES];
 
-     // Read and store the quantum gate matrices in a 2-D array called gates
-     // Each matrix is reprsented like this:
-     // [a,b,c,d] = a  b
-     //             c  d
-     for (int i=0; i < NUM_QUANTUM_GATES; i++){
-        for (int j=0; j < QUANTUM_GATE_SIZE; j++){
-            int r = fscanf(in_file, "%s", &input_elem[0]);
-            if (r == EOF){
-                printf("Incorrect input formatting. Abort\n");
-                return 1;
-            }
-            gates[i][j] = atof(input_elem);
-        } 
+    // equivalent to saying if ( in_file == NULL )
+     if (!in_file){
+        printf("oops, file can't be read\n");
+        exit(-1);
      }
 
-    // Read the rest of the file 
+    FILE* in_file = fopen(argv[1], "r");                   // read only
+    char input_elem[32];                                   // arbitrary length
+
+    // Read and store the quantum gate matrices in a 2-D array called gates
+    // Each matrix is reprsented like this:
+    // [a,b,c,d] = a  b
+    //             c  d
+    for (int i=0; i < NUM_QUANTUM_GATES; i++){
+       for (int j=0; j < QUANTUM_GATE_SIZE; j++){
+           int r = fscanf(in_file, "%s", &input_elem[0]);
+           if (r == EOF){
+               printf("Incorrect input formatting. Abort\n");
+               return 1;
+           }
+           gates[i][j] = atof(input_elem);
+       }
+    }
+
+    // Read the rest of the file
     int max_vector_size=pow(2,30);
     float* state_vector  = (float*)malloc(max_vector_size * sizeof(float));
     int count = 0;
@@ -86,11 +92,11 @@ int main(int argc, char **argv){
     int tmp;
     for (int i=0; i<NUM_QUANTUM_GATES; i++){
         tmp =  (int)state_vector[count-i-1];
-        T_BITS[NUM_QUANTUM_GATES - i - 1] = tmp; 
+        T_BITS[NUM_QUANTUM_GATES - i - 1] = tmp;
     }
 
     /////////////////////////////////////////////////////////////////
-    //                    CUDA Error Detection                     // 
+    //                    CUDA Error Detection                     //
     /////////////////////////////////////////////////////////////////
 
     // Error code to check return values for CUDA calls
@@ -115,7 +121,7 @@ int main(int argc, char **argv){
 
     // Initialize the host input vectors
     for (int i = 0; i < numElements; ++i){
-        h_A[i] = state_vector[i]; 
+        h_A[i] = state_vector[i];
     }
 
     // Allocate the device input vector A
