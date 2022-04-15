bf9372f6a00f0e16a18583a62a3bb300c8dd5dc8
Sirajus Salekin <ssalek14@earlham.edu>
2022-04-15 17:58:13 +0000

    Copied off version1 to implement SharedMem optimization

--- quamsimV2.cu
+++ quamsimV2.cu
@@ -9,18 +9,9 @@
  *
  */
 
-/**
- * Vector addition: C = A + B.
- *
- * This sample is a very basic sample that implements element by element
- * vector addition. It is the same as the sample illustrating Chapter 2
- * of the programming guide with some additions like error checking.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 // For the CUDA runtime routines (prefixed with "cuda_")
 #include <cuda_runtime.h>
 
@@ -28,11 +19,12 @@
 /**
  * CUDA Kernel Device code
  *
- * Computes the vector addition of A and B into C. The 3 vectors have the same
- * number of elements numElements.
+ * Performs application of quantum gate to the quantum state supplied in
+ * A. C contains the state after the application. 
  */
-// A contains input
+
 __global__ void matrix_mul(float *A, float *C, float a, float b, float c, float d, int state_size, int t_bit){
+    // A contains input
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
     //i=x1, flipped=x2
@@ -43,65 +35,75 @@ __global__ void matrix_mul(float *A, float *C, float a, float b, float c, float 
             C[flipped] = (A[i] * c ) + (A[flipped] * d);
         }
     }
-   
 }
 
 
 /**
  * Host main routine
  */
-int
-main(int argc, char **argv)
-{
-    /*
-        INPUT READING FROM FILE
-    */
-     float a, b, c, d; // quantum gate matrix 
-     int t_bit; 
-     int vector_size=pow(2,30), final_size;
-     float*  state_vector  = (float*)malloc(vector_size * sizeof(float));
 
-     float number;
-      FILE* in_file = fopen(argv[1], "r"); // read only  
-     
-      if (! in_file ) // equivalent to saying if ( in_file == NULL ) 
-         {  
-            printf("oops, file can't be read\n"); 
-            exit(-1); 
-         } 
+int main(int argc, char **argv){
+    /////////////////////////////////////////////////////////////////
+    //                    Reading From Input                       //
+    /////////////////////////////////////////////////////////////////
+    // How many quantum gate matrices we will have? variable or fixed?
+    // Six - fixed
+    int NUM_QUANTUM_GATES = 6;
+    int QUANTUM_GATE_SIZE = 4;
+    float gates[NUM_QUANTUM_GATES][QUANTUM_GATE_SIZE];
+    int T_BITS[NUM_QUANTUM_GATES];
 
-      // attempt to read the next line and store 
-      // the value in the "number" variable 
-      int count = 0;
-      float last;
-      while ( fscanf(in_file, "%f", &number ) != EOF )  
-         { 
-           if (count==0) a = number;
-           else if (count==1) b = number;
-           else if (count==2) c = number;
-           else if (count==3) d = number;
+    FILE* in_file = fopen(argv[1], "r");                   // read only
+    // equivalent to saying if ( in_file == NULL )
+     if (!in_file){
+        printf("oops, file can't be read\n");
+        exit(-1);
+     }
 
-           else {
-               state_vector[count-4] = number; 
-            }
+    // Read and store the quantum gate matrices in a 2-D array called gates
+    // Each matrix is reprsented like this:
+    // [a,b,c,d] = a  b
+    //             c  d
+    char input_elem[32];                                   // arbitrary length
+    for (int i=0; i < NUM_QUANTUM_GATES; i++){
+       for (int j=0; j < QUANTUM_GATE_SIZE; j++){
+           int r = fscanf(in_file, "%s", &input_elem[0]);
+           if (r == EOF){
+               printf("Incorrect input formatting. Abort\n");
+               return 1;
+           }
+           gates[i][j] = atof(input_elem);
+       }
+    }
 
-           last = number;
-           count++;
-         } 
-           
-     t_bit = (int) last;
-     final_size = count - 5;
-    //printf("Qubit %d size: %d a:%f, b:%f c:%f d:%f\n", qubit, final_size, a, b, c, d);
-    /* END READING FROM INPUT */
+    // Read the rest of the file
+    int max_vector_size=pow(2,30);
+    float* state_vector  = (float*)malloc(max_vector_size * sizeof(float));
+    int count = 0;
+    while (fscanf(in_file, "%s", &input_elem[0]) != EOF){
+        state_vector[count] = atof(input_elem);
+        count++;
+    }
 
+    // Go to the back of the state_vector, and grabs the
+    // last NUM_QUANTUM_GATES elements, convert them into ints
+    // and store them in an array
+    int tmp;
+    for (int i=0; i<NUM_QUANTUM_GATES; i++){
+        tmp =  (int)state_vector[count-i-1];
+        T_BITS[NUM_QUANTUM_GATES - i - 1] = tmp;
+    }
+
+    /////////////////////////////////////////////////////////////////
+    //                    CUDA Error Detection                     //
+    /////////////////////////////////////////////////////////////////
 
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
 
     // Print the vector length to be used, and compute its size
-    int numElements = final_size;
+    int numElements = count - NUM_QUANTUM_GATES;
     size_t size = numElements * sizeof(float);
-    //printf("[Vector addition of %d elements]\n", numElements);
 
     // Allocate the host input vector A
     float *h_A = (float *)malloc(size);
@@ -110,24 +112,21 @@ main(int argc, char **argv)
     float *h_C = (float *)malloc(size);
 
     // Verify that allocations succeeded
-    if (h_A == NULL || h_C == NULL)
-    {
+    if (h_A == NULL || h_C == NULL){
         fprintf(stderr, "Failed to allocate host vectors!\n");
         exit(EXIT_FAILURE);
     }
 
     // Initialize the host input vectors
-    for (int i = 0; i < numElements; ++i)
-    {
-        h_A[i] = state_vector[i]; 
+    for (int i = 0; i < numElements; ++i){
+        h_A[i] = state_vector[i];
     }
 
     // Allocate the device input vector A
     float *d_A = NULL;
-    err = cudaMallocManaged((void **)&d_A, size);
+    err = cudaMalloc((void **)&d_A, size);
 
-    if (err != cudaSuccess)
-    {
+    if (err != cudaSuccess){
         fprintf(stderr, "Failed to allocate device vector A (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
@@ -135,7 +134,7 @@ main(int argc, char **argv)
 
     // Allocate the device output vector C
     float *d_C = NULL;
-    err = cudaMallocManaged((void **)&d_C, size);
+    err = cudaMalloc((void **)&d_C, size);
 
     if (err != cudaSuccess)
     {
@@ -143,40 +142,52 @@ main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    // Copy the host input vectors A and B in host memory to the device input vectors in
-    // device memory
-    //printf("Copy input data from the host memory to the CUDA device\n");
-    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    int round=0;
+    while (round < 6){
+        float a,b,c,d;
+        a=gates[round][0];
+        b=gates[round][1];
+        c=gates[round][2];
+        d=gates[round][3];
+        int t_bit = T_BITS[round];
 
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
+        // Copy the host input vectors A and B in host memory to the device input vectors in
+        // device memory
+        err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
 
 
-    // Launch the Vector Add CUDA Kernel
-    int threadsPerBlock = 256;
-    int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
-    //printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-    matrix_mul<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_C, a, b, c, d, numElements, t_bit);
-    err = cudaGetLastError();
+        // Launch the Vector Add CUDA Kernel
+        int threadsPerBlock = 256;
+        int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
+        matrix_mul<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_C, a, b, c, d, numElements, t_bit);
+        err = cudaGetLastError();
 
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
 
-    // Copy the device result vector in device memory to the host result vector
-    // in host memory.
-    //printf("Copy output data from the CUDA device to the host memory\n");
-    err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+        // Copy the device result vector in device memory to the host result vector
+        // in host memory.
+        err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+        // Important: this takes the output from the last round, and copies it
+        // over to the input array of the next round.
+        memcpy(h_A, h_C, size);
+        round++;
     }
 
     // Verify that the result vector is correct
